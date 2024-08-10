ffb6a20ec768437a65d0b1115e22abef88ad91b7
Yermehyaw <yermehyaw@gmail.com>
2024-08-10 02:01:17 +0100

    Make 1-hbnb.html dynamic

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,29 @@
+/**
+ * make template/1-hbnb.html dynamic
+ */
+
+const $ = window.$;
+
+$(document).ready(function () {
+  let amenityDict = {};
+  $('li>input').toggle(function () {
+    const amenityId = $(this).attr('data-id');
+    const amenityName = $(this).attr('data-name');
+    amenityDict[amenityId] = amenityName; // add the ticked amenity to dict
+    writeAmenity(amenityDict);
+  }, function () {
+    const amenityId = $(this).attr('data-id');
+    const amenityName = $(this).attr('data-name');
+    delete amenityDict.amenityId; // remove the unticked amenity from dict
+    writeAmenity(amenityDict);
+  });
+  function writeAmenity(amenityDict) {
+  /**
+   * write a dict of amenities to an elem
+   */
+  $('div>h4').empty(); // clear all existing values in the elem
+  for (let amenity in amenityDict)
+    const amenityName = amenityDict[amenity];
+    $('div>h4').text(amenityName);
+}
+});
