632dd03d673433d1ba093e050628f2f76d2e4ddd
Yermehyaw <yermehyaw@gmail.com>
2024-08-12 14:53:24 +0100

    Fix code struct

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -4,6 +4,16 @@
 
 const $ = window.$;
 
+function writeAmenity(amenityDict) {
+  /**
+   * write a dict of amenities to an elem
+   */
+  $('div>h4').empty(); // clear all existing values in the elem
+  for (let amenity in amenityDict)
+    const amenityName = amenityDict[amenity];
+    $('div>h4').text(amenityName);
+}
+
 $(document).ready(function () {
   let amenityDict = {};
   $('li>input').toggle(function () {
@@ -17,13 +27,4 @@ $(document).ready(function () {
     delete amenityDict.amenityId; // remove the unticked amenity from dict
     writeAmenity(amenityDict);
   });
-  function writeAmenity(amenityDict) {
-  /**
-   * write a dict of amenities to an elem
-   */
-  $('div>h4').empty(); // clear all existing values in the elem
-  for (let amenity in amenityDict)
-    const amenityName = amenityDict[amenity];
-    $('div>h4').text(amenityName);
-}
 });
